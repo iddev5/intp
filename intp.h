b8b7a9f957d37fae9937ebd2058a04080f57c58a
AyushGameDev <ayushtechportal@gmail.com>
2019-11-12 12:47:33 +0530

    Added to GitHub

---
+++ intp.h
@@ -0,0 +1,66 @@
+#ifndef INTP_H
+#define INTP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//////////////////////////////
+//----------Utilities---------
+//////////////////////////////
+
+static int intp_is_space(char c) {
+    return (c == ' ');
+}
+
+static int intp_is_alpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+static int intp_is_num(char c) {
+    return (c >= '0' && c <= '9');
+}
+
+static int intp_is_id(char c) {
+    return intp_is_alpha(c) || c == '_';
+}
+
+/*
+//----------Utilities---------
+int intp_is_space(char c);
+int intp_is_alpha(char c);
+int intp_is_num(char c);
+int intp_is_id(char c);
+*/
+
+//----------Enums-------------
+enum intp__token_type {
+    hex,
+    bin,
+    num,
+    al,
+
+    op_add,
+    op_sub,
+    op_mul,
+    op_div,
+    op_inc,
+    op_dec
+};
+
+//----------Structs-----------
+typedef struct {
+    char *data;
+    char *tok;
+    char *word;
+    /**<----->*/
+} intp_info;
+
+//----------Main--------------
+int intp_init(intp_info *info);
+int intp_free(intp_info *info);
+
+void intp_string(intp_info *info , char *str);
+void intp_file(intp_info *info, char *fn);
+
+#endif
---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "intp.h"
+
+int main(int argc, char **argv) {
+
+	if(argc < 2) {
+        printf("Usage: INTP filename.intp\n");
+        exit(-1);
+	}
+
+	int r;
+    intp_info info;
+
+    r = intp_init(&info);
+
+    if(r)
+        intp_file(&info, argv[1]);
+}
